f7006e284f4f407a925f415a0b702665333c16fa
eolenja <167790428+eolenja@users.noreply.github.com>
2024-08-06 12:06:40 -0400

    first commit

---
+++ src/QUESTION2.java
@@ -0,0 +1,10 @@
+public class QUESTION2 {
+    public static void main(String[] args) {
+        double dub1 = 1.2;
+        double dub2 = 2.1;
+
+        double dubSum = dub1 + dub2;
+        System.out.println(dubSum);
+
+    }
+}
---
+++ src/QUESTION3.java
@@ -0,0 +1,10 @@
+public class QUESTION3 {
+    public static void main(String[] args) {
+        int myint = 1;
+        double mydouble = 2.1;
+
+        double sumOfIntAndDub = myint + mydouble;
+        System.out.println(sumOfIntAndDub);
+
+    }
+}
